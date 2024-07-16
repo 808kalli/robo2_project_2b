660ef54ed10c8ae7a1087601f9119327365eb272
808kalli <eliaskallioras@gmail.com>
2024-07-17 01:28:15 +0300

    created update step

--- robo2_mobile/src/kalman.py
+++ robo2_mobile/src/kalman.py
@@ -49,13 +49,25 @@ class KalmanFilterNode():
         self.x = np.array([[0.0], [0.0], [0.0]])
         self.vel = 0.25
 
-        self.H = np.array([[1, 0]])
-        self.Q = np.array([[0.01, 0], [0, 0.01]])
-        self.R = np.array([[0.1]])
-        x0 = np.array([[0], [1]])
-        P0 = np.array([[1, 0], [0, 1]])
+        #Which wall each sonar is pointing to, 0 means no wall detected (distance >= 2.0)
+        self.wall_f = 0
+        self.wall_fl = 0
+        self.wall_l = 0
+        self.wall_fr = 0
+        self.wall_r = 0
 
-        self.u = np.array([[0]])  # Control input, assuming zero for simplicity
+        self.H = np.array([[1.0, 0.0, 0.0],
+                           [0.0, 1.0, 0.0],
+                           [0.0, 0.0, 1.0]])
+        
+        self.R = np.array([[0.0001, 0.0, 0.0],  #sensor covarriance matrix
+                           [0.0, 0.0001, 0.0],
+                           [0.0, 0.0, 0.00004]])
+
+        self.P = np.array([[0.00001, 0, 0],     #starting P array, we are very ceratin about the starting position
+                           [0, 0.00001, 0],
+                           [0, 0, 0.00001]])
+
         self.z = np.array([[0.0], [0.0], [0.0]])
 
 
@@ -69,7 +81,8 @@ class KalmanFilterNode():
         self.velocity_sub = rospy.Subscriber('/mymobibot/cmd_vel', Twist, self.vel_callback)
 
         #PUBLISH
-        self.pub = rospy.Publisher('/filtered_pose', PoseStamped, queue_size=10)
+        self.pose_pub = rospy.Publisher('/filtered_pose', PoseStamped, queue_size=10)
+        self.yaw_pub = rospy.Publisher('/filtered_yaw', Float64, queue_size=10)
 
         self.publish()
 
@@ -110,50 +123,171 @@ class KalmanFilterNode():
             self.vel = 0.25
         self.x[2] = (self.x[2] + pi) % (2*pi) - pi      #normalize angle
 
-        self.A = np.array([[1], [0], [-self.vel*dt*sin(self.x[2]) - 0.5*self.imuLinAccX*(dt**2)*sin(self.x[2])],
-                           [0], [1], [self.vel*dt*cos(self.x[2]) + 0.5*self.imuLinAccX*(dt**2)*cos(self.x[2])],
-                           [0], [0], [1]])
+        self.A = np.array([[1, 0, -self.vel*dt*sin(self.x[2]) - 0.5*self.imuLinAccX*(dt**2)*sin(self.x[2])],
+                           [0, 1, self.vel*dt*cos(self.x[2]) + 0.5*self.imuLinAccX*(dt**2)*cos(self.x[2])],
+                           [0, 0, 1]])
         
-        self.Cw = np.array([[0.00001], [0], [0],
-                           [0], [0.00001], [0],
-                           [0], [0], [0.0000002]])
-        
+        self.Cw = np.array([[0.00001, 0, 0],
+                           [0, 0.00001, 0],
+                           [0, 0, 0.0000002]])
+                
         self.P = self.A@self.P@self.A.T + self.Cw
 
         return self.x, self.P
 
-    def update(self):
+    def update(self, sonar_front, sonar_front_left, sonar_front_right, sonar_left, sonar_right):
+
+        angle_fl = (self.x[2] + pi/4 + pi) % (2*pi) - pi
+        angle_l = (self.x[2] + pi/2 + pi) % (2*pi) - pi
+        angle_fr = (self.x[2] - pi/4 + pi) % (2*pi) - pi
+        angle_r = (self.x[2] - pi/2 + pi) % (2*pi) - pi
+        
+        top_left = pi/2 - atan2((2 - self.x[0]), (2 - self.x[1]))
+        bottom_left = pi/2 + atan2((-2 - self.x[0]), (2 - self.x[1]))
+        top_right = 2*pi - atan2((-2 - self.x[1]), (2 - self.x[0]))
+        bottom_right = pi + atan2((-2 -self.x[1]), (-2 - self.x[0]))
+        top_left = (top_left + pi) % (2*pi) - pi                #normalize angle
+        bottom_left = (bottom_left + pi) % (2*pi) - pi          #normalize angle
+        top_right = (top_right + pi) % (2*pi) - pi              #normalize angle
+        bottom_right = (bottom_right + pi) % (2*pi) - pi        #normalize angle
+
+        #front sonar wall
+        if (self.x[2] <= top_left and self.x[2] > top_right):               #wall 1
+            self.wall_f = 1
+        elif (self.x[2] <= bottom_left and self.x[2] > top_left):           #wall 2
+            self.wall_f = 2
+        elif (self.x[2] <= bottom_right and self.x[2] > bottom_left):       #wall 3
+            self.wall_f = 3
+        elif (self.x[2] <= top_right and self.x[2] > bottom_right):         #wall 4
+            self.wall_f = 4
+
+        #front_left sonar wall
+        if (angle_fl <= top_left and angle_fl > top_right):               #wall 1
+            self.wall_fl = 1
+        elif (angle_fl <= bottom_left and angle_fl > top_left):           #wall 2
+            self.wall_fl = 2
+        elif (angle_fl <= bottom_right and angle_fl > bottom_left):       #wall 3
+            self.wall_fl = 3
+        elif (angle_fl <= top_right and angle_fl > bottom_right):         #wall 4
+            self.wall_fl = 4
+
+        #left sonar wall
+        if (angle_l <= top_left and angle_l > top_right):               #wall 1
+            self.wall_l = 1
+        elif (angle_l <= bottom_left and angle_l > top_left):           #wall 2
+            self.wall_l = 2
+        elif (angle_l <= bottom_right and angle_l > bottom_left):       #wall 3
+            self.wall_l = 3
+        elif (angle_l <= top_right and angle_l > bottom_right):         #wall 4
+            self.wall_l = 4
+
+        #front_right sonar wall
+        if (angle_fr <= top_left and angle_fr > top_right):               #wall 1
+            self.wall_fr = 1
+        elif (angle_fr <= bottom_left and angle_fr > top_left):           #wall 2
+            self.wall_fr = 2
+        elif (angle_fr <= bottom_right and angle_fr > bottom_left):       #wall 3
+            self.wall_fr = 3
+        elif (angle_fr <= top_right and angle_fr > bottom_right):         #wall 4
+            self.wall_fr = 4
+
+        #right sonar wall
+        if (angle_r <= top_left and angle_r > top_right):               #wall 1
+            self.wall_r = 1
+        elif (angle_r <= bottom_left and angle_r > top_left):           #wall 2
+            self.wall_r = 2
+        elif (angle_r <= bottom_right and angle_r > bottom_left):       #wall 3
+            self.wall_r = 3
+        elif (angle_r <= top_right and angle_r > bottom_right):         #wall 4
+            self.wall_r = 4
 
         
+        #find position based on measurements
+        #wall 1
+        if (sonar_front < 1.5 and self.wall_f == 1):
+            d1 = sonar_front*abs(cos(self.x[2]))
+            self.z[0][0] = 2 - d1
+        elif (sonar_front_left < 1.5 and self.wall_fl == 1):
+            d1 = sonar_front_left*abs(cos(self.x[2]))
+            self.z[0][0] = 2 - d1
+        elif (sonar_front_right < 1.5 and self.wall_fr == 1):
+            d1 = sonar_front_right*abs(cos(self.x[2]))
+            self.z[0][0] = 2 - d1
+        elif (sonar_left < 1.5 and self.wall_l == 1):
+            d1 = sonar_left*abs(cos(self.x[2]))
+            self.z[0][0] = 2 - d1
+        elif (sonar_right < 1.5 and self.wall_r == 1):
+            d1 = sonar_right*abs(cos(self.x[2]))
+            self.z[0][0] = 2 - d1
+        #wall 3
+        elif (sonar_front < 1.5 and self.wall_f == 3):
+            d1 = sonar_front*abs(cos(self.x[2]))
+            self.z[0][0] = -2 + d1
+        elif (sonar_front_left < 1.5 and self.wall_fl == 3):
+            d1 = sonar_front_left*abs(cos(self.x[2]))
+            self.z[0][0] = -2 + d1
+        elif (sonar_front_right < 1.5 and self.wall_fr == 3):
+            d1 = sonar_front_right*abs(cos(self.x[2]))
+            self.z[0][0] = -2 + d1
+        elif (sonar_left < 1.5 and self.wall_l == 3):
+            d1 = sonar_left*abs(cos(self.x[2]))
+            self.z[0][0] = -2 + d1
+        elif (sonar_right < 1.5 and self.wall_r == 3):
+            d1 = sonar_right*abs(cos(self.x[2]))
+            self.z[0][0] = -2 + d1
+        else:
+            d1 = None
 
+        #wall 2
+        if (sonar_front < 1.5 and self.wall_f == 2):
+            d2 = sonar_front*abs(sin(self.x[2]))
+            self.z[1][0] = 2 - d2
+        elif (sonar_front_left < 1.5 and self.wall_fl == 2):
+            d2 = sonar_front_left*abs(sin(self.x[2]))
+            self.z[1][0] = 2 - d2
+        elif (sonar_front_right < 1.5 and self.wall_fr == 2):
+            d2 = sonar_front_right*abs(sin(self.x[2]))
+            self.z[1][0] = 2 - d2
+        elif (sonar_left < 1.5 and self.wall_l == 2):
+            d2 = sonar_left*abs(sin(self.x[2]))
+            self.z[1][0] = 2 - d2
+        elif (sonar_right < 1.5 and self.wall_r == 2):
+            d2 = sonar_right*abs(sin(self.x[2]))
+            self.z[1][0] = 2 - d2
+        #wall 4
+        elif (sonar_front < 1.5 and self.wall_f == 4):
+            d2 = sonar_front*abs(sin(self.x[2]))
+            self.z[1][0] = -2 + d2
+        elif (sonar_front_left < 1.5 and self.wall_fl == 4):
+            d2 = sonar_front_left*abs(sin(self.x[2]))
+            self.z[1][0] = -2 + d2
+        elif (sonar_front_right < 1.5 and self.wall_fr == 4):
+            d2 = sonar_front_right*abs(sin(self.x[2]))
+            self.z[1][0] = -2 + d2
+        elif (sonar_left < 1.5 and self.wall_l == 4):
+            d2 = sonar_left*abs(sin(self.x[2]))
+            self.z[1][0] = -2 + d2
+        elif (sonar_right < 1.5 and self.wall_r == 4):
+            d2 = sonar_right*abs(sin(self.x[2]))
+            self.z[1][0] = -2 + d2
+        else:
+            d2 = None
 
+        self.z[2][0] = self.imu_yaw
 
-
-        y = z - self.H @ self.x
-        S = self.H @ self.P @ self.H.T + self.R
-        K = self.P @ self.H.T @ np.linalg.inv(S)
-        self.x = self.x + K @ y
-        self.P = self.P - K @ self.H @ self.P
+        #measurement model
+        if ((d1 is not None) and (d2 is not None)):
+            y = self.z - self.H @ self.x
+            S = self.H @ self.P @ self.H.T + self.R
+            K = self.P @ self.H.T @ np.linalg.inv(S)
+            self.x = self.x + K @ y
+            self.P = self.P - K @ self.H @ self.P
+        
         return self.x, self.P
 
     def publish(self):
 
-        fsfw = 0.35
-        flsfw = 0.25
-        frsfw = 0.25
-
-        phase = 1
-
         dt = 0.1
-
-        dax = 0.002
-        dwz = 0.002
-
-        # estimated errors
-        dth = dwz*dt
-        dvx = dax*dt
-        dx  = 1.0/2*dax*np.cos(self.imu_yaw)*dt*dt
-        dy  = 1.0/2*dax*np.sin(self.imu_yaw)*dt*dt
 
         print("Executing Extended Kalman Filter...")
 
@@ -165,49 +299,23 @@ class KalmanFilterNode():
             sonar_left = self.sonar_L.range
             sonar_right = self.sonar_R.range
 
-            self.x = self.predict(dt)
+            (self.x, self.P) = self.predict(dt)
+            (self.x, self.P) = self.update(sonar_front, sonar_front_left, sonar_front_right, sonar_left, sonar_right)     
 
-            if (phase == 1):
-                if ((sonar_front < fsfw) or (sonar_front_left < flsfw) or (sonar_front_right < frsfw)):
-                    phase = 2
-            if (phase == 2):
-                if (sonar_front < 2.0) and ((sonar_left < 2.0) or (sonar_right < 2.0)):
-                    d1 = sonar_front*abs(cos(self.imu_yaw))
-                    d2 = sonar_left*abs(cos(self.imu_yaw)) if sonar_left < sonar_right else sonar_right*abs(cos(self.imu_yaw))
-                else:
-                    d1 = None
-                    d2 = None
-
-                if ((d1 is not None) and (d2 is not None)):
-                    if ((self.x[0] > 0) and (self.x[1] > 0)):       #Q2  
-                        self.z = np.array([[2-d1], [2-d2], [self.imu_yaw]])
-                    if ((self.x[0] < 0) and (self.x[1] > 0)):       #Q3  
-                        self.z = np.array([[-2+d1], [2-d2], [self.imu_yaw]])
-                    if ((self.x[0] > 0) and (self.x[1] < 0)):       #Q1  
-                        self.z = np.array([[2-d1], [-2+d2], [self.imu_yaw]])
-                    if ((self.x[0] < 0) and (self.x[1] < 0)):       #Q4  
-                        self.z = np.array([[-2+d1], [-2+d2], [self.imu_yaw]])     
-
-            # print(self.z)       
-
-
-            # #EKF 
-            # self.kf.predict(self.u)
-            # x, P = self.kf.update(measurement)
-
-            # # Create PoseStamped message
+            #create coordinates message
             pose_msg = PoseStamped()
             pose_msg.header.stamp = rospy.Time.now()
             pose_msg.header.frame_id = "map"
             pose_msg.pose.position.x = self.x[0]
             pose_msg.pose.position.y = self.x[1]
-            # pose_msg.pose.position.z = 0
-            # pose_msg.pose.orientation.x = 0
-            # pose_msg.pose.orientation.y = 0
-            # pose_msg.pose.orientation.z = 0
-            # pose_msg.pose.orientation.w = 1
 
-            self.pub.publish(pose_msg)
+            #create yaw message
+            yaw_msg = Float64()
+            yaw_msg.data = self.x[2]
+
+            #publish
+            self.pose_pub.publish(pose_msg)
+            self.yaw_pub.publish(yaw_msg)
 
             self.pub_rate.sleep()
 
